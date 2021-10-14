0a3c75922c50c89b56c585ffe455d23edcc1c049
kmitao <92508889+kmitao@users.noreply.github.com>
2021-10-14 15:10:36 +0800

    Add files via upload

---
+++ need/bottom.js
@@ -0,0 +1,475 @@
+// 公告
+var notice = '本站所有产品均已经过严格检测，保证无毒，请放心下载使用！';
+
+// 热门推荐
+var grid_data_1 = [
+    
+    {
+        url: 'https://cpa4615.iqy24yt.com',
+        text: '私密直播',
+        img: 'https://hd.cdn8-video.com/005453453.gif',
+    }, 
+    {
+        url: 'https://sp136.xyz/?channelCode=lee04',
+        text: '蜻蜓直播',
+        img: 'https://hd.cdn8-video.com/metoo.webp',
+    }, 
+    {
+        url: 'http://xc.lszgf.top/web.html?channelCode=yz07_12',
+        text: '新茶直播',
+        img: 'https://hd.cdn8-video.com/rgw52.gif',
+    },
+    {
+        url: 'https://dx013.live/?channelCode=9FFF6',
+        text: '大秀直播',
+        img: 'https://hd.cdn8-video.com/uty52.gif',
+    }, 
+    {
+        url: 'https://manguan.vip?channel=LEE791',
+        text: '春茶社区',
+        img: 'https://hd.cdn8-video.com/eghfdfasgdhfjgh.gif',
+    },
+    /*{
+        url: 'http://8899p.xyz/?channelCode=zhang-5',
+        text: '邂逅社区',
+        img: 'https://hd.cdn8-video.com/s248.gif',
+    },*/
+    {
+        url: 'http://912k.cc/?channelCode=10331',   
+        text: '青橙直播',
+        img: 'https://hd.cdn8-video.com/ghg35.gif',
+    },
+    
+    {
+        url: 'https://51mh049.com/?_c=cpa11mh',
+        text: '成人动漫',
+        img: 'https://hd.cdn8-video.com/ertoiuytr.gif',
+    },
+    {
+        url: 'https://dysp9.me?channel=tdy914j10',
+        text: '抖阴视频',
+        img: 'https://hd.cdn8-video.com/asdfghfdsa.gif',
+    },
+    {
+        url: 'https://sdd911.com/share?code=001M4PA8X',
+        text: '迪丽热巴',
+        img: 'https://hd.cdn8-video.com/s199.gif',
+    },
+    {
+        url: 'http://blabc05.com/?dc=bxza7604',   
+        text: '逼哩逼哩',
+        img: 'https://hd.cdn8-video.com/dfghjytredfgfx.gif',
+    },
+    {
+        url: 'https://63she.me/?_c=ym107c',   
+        text: '暗网处女',
+        img: 'https://hd.cdn8-video.com/53450.gif',
+    },
+    {
+        url: 'https://9191n.net?_chan=xjkmt930-4',
+        text: '91香蕉',
+        img: 'https://hd.cdn8-video.com/ynfl.gif.gif',
+    },
+    {
+        url: 'http://yt.yingtaotv3.work/?a=ngr684',   
+        text: '樱桃视频',
+        img: 'https://hd.cdn8-video.com/x1.webp',
+    }, 
+    {//包月
+        url: 'https://www.112241.vip/',
+        text: '约炮平台',
+        img: 'https://hd.cdn8-video.com/thdthdhdjdj.gif',
+    },
+    
+    {
+        url: 'https://web.zhengzhouybdl.com/?channelCode=JY96',
+        text: '91视频',
+        img: 'https://hd.cdn8-video.com/s245.gif',
+    },
+    {
+        url: 'https://ldyljr108.net/162e42c5fbbfd90b',
+        text: '绿巨人',
+        img: 'https://hd.cdn8-video.com/efefdvdftergfngfh.gif',
+    },
+    {
+        url: 'https://91lu2015.com/jwknb',
+        text: '91撸视频',
+        img: 'https://hd.cdn8-video.com/drhdjykesgf.gif',
+    },
+    {
+        url: 'https://xb16.me/?_c=ocmt4xb',
+        text: '杏吧视频',
+        img: 'https://hd.cdn8-video.com/345675434.gif',
+    },
+    {
+        url: 'https://tm10ji.com/Hd6s67.html?code=8UAZU',   
+        text: '春水堂视频',
+        img: 'https://hd.cdn8-video.com/qingqingcao.gif',
+    }, 
+    {
+        url: 'https://dyfactor.com?channel=tdy10132',   
+        text: '抖音性世界',
+        img: 'https://hd.cdn8-video.com/sdgfdswe.gif',
+    },
+
+
+
+
+
+];
+
+// 赚钱娱乐
+var grid_data_2 = [
+      
+     {
+        url: 'https://333kqp.cc',
+        text: '开元棋牌',
+        img: 'https://hd.cdn8-video.com/s543.jpg',
+    },
+    {
+        url: 'https://pj7873.com',
+        text: '新葡京赌场',
+        img: 'https://hd.cdn8-video.com/gdhfgjgk.jpg',
+    },
+    {
+        url: 'https://www.99950hg6.com/#/register',
+        text: '澳门皇冠',
+        img: 'https://hd.cdn8-video.com/s864.jpg',
+    },
+    {
+        url: 'https://4593vip.com/?shareName=www.qq2287.com',
+        text: '澳门金沙',
+        img: 'https://hd.cdn8-video.com/fkhgfdsfgh.jpg',
+    }, 
+    
+    
+
+];
+
+// 直播APP
+var grid_data_3 = [
+    {//2-1
+        url: 'https://cpa4615.iqy24yt.com',
+        text: '私密直播',
+        img: 'https://hd.cdn8-video.com/s120.gif',
+    },
+    {
+        url: 'https://sp136.xyz/?channelCode=lee04',
+        text: '蜻蜓直播',
+        img: 'https://hd.cdn8-video.com/shangmengyuepao.gif',
+    },
+    {//1-1
+        url: 'http://xc.lszgf.top/web.html?channelCode=yz07_12',
+        text: '新茶直播',
+        img: 'https://hd.cdn8-video.com/dhjgkjjhs.gif',
+        
+    },
+    {
+        url: 'https://dx013.live/?channelCode=9FFF6',
+        text: '大秀直播',
+        img: 'https://hd.cdn8-video.com/s135.gif',
+    },
+    {
+        url: 'http://912k.cc/?channelCode=10331',
+        text: '青橙直播',
+        img: 'https://hd.cdn8-video.com/7.gif',
+    },
+    
+    
+    /*{
+        url: 'https://9191n.net?_chan=xjkmt930-4',
+        text: '香蕉直播',
+        img: 'https://hd.cdn8-video.com/s135.gif',
+    },
+    {
+        url: 'https://sdd911.com/share?code=001M4PA8X',
+        text: '私密直播',
+        img: 'https://hd.cdn8-video.com/shangmengyuepao.gif',
+    },*/
+    
+    
+    /*{//1-3
+        url: 'https://51mh049.com/?_c=cpa11mh',
+        text: '百合直播',
+        img: 'https://hd.cdn8-video.com/s119.gif',
+    },
+    {//2-4
+        url: 'https://ljrdown103.net/162e42c5fbbfd90b',
+        text: '约爱直播',
+        img: 'https://hd.cdn8-video.com/s176.gif',
+    },*/
+    /*{//1-2
+        url: 'https://rrtv.532279.com/?channelCode=rrxx',
+        text: '软软直播',
+        img: 'https://hd.cdn8-video.com/s114.gif',
+    }, */
+    
+    
+
+
+    
+
+];
+
+// 视频APP
+var grid_data_4 = [
+    
+
+
+];
+
+// 在线视频
+var grid_data_5 = [
+    {
+        url: 'https://igao28.com/html/new_index.html',
+        text: '爱搞视频',
+        img: 'https://hd.cdn8-video.com/x7.webp',
+        hot: 1,
+        title: '在线观看',
+    },
+    {
+        url: 'https://xxdd19.cc/html/index_m.html',
+        text: '羞羞答答',
+        img: 'https://hd.cdn8-video.com/x1.webp',
+        title: '在线观看',
+    }, 
+    {
+        url: 'https://seyoyo10.com/index/home.html',
+        text: '色哟哟',
+        img: 'https://hd.cdn8-video.com/qwtrwyewtw.gif',
+        
+    }, 
+    {
+        url: 'https://sebige999.com/',
+        text: '成人b站',
+        img: 'https://hd.cdn8-video.com/momo.webp',
+    },
+    
+    /*{
+        url: 'https://dysp9.me?channel=tdy914j10',
+        text: '抖阴视频',
+        img: 'https://hd.cdn8-video.com/sghasgh.gif',
+    },
+    {
+        url: 'https://91lu2015.com/jwknb',
+        text: '啪啪视频',
+        img: 'https://hd.cdn8-video.com/7.gif',   
+    },
+    {
+        url: 'http://hg456jikv.pni3rj.online/sgo0971.html?pkg=cuke0421&inviteCode=subtone000',
+        text: '黄瓜视频',
+        img: 'https://hd.cdn8-video.com/s123.gif',
+    },
+    {
+        url: 'https://anwqko.com?android=damao1008&type=1&ios=damao1008&all=&style=0',
+        text: '成人动漫',
+        img: 'https://hd.cdn8-video.com/shangmengyuepao.gif',
+    },*/
+    
+
+];
+
+// 横幅
+var bn_data_1 = [
+    /*{
+        url: 'https://www.99950hg6.com/#/register',
+        text: '欧洲杯',
+        img: 'https://hd.cdn8-video.com/gdgg.gif',
+    }*/
+];
+
+// 底漂
+var bn_bottom_data = [
+    {
+        url: 'https://igao28.com/html/new_index.html',
+        img: '/need/images/igao.gif?3',
+    }
+    
+
+
+];
+
+// 随机图片
+var grid_data_randomimg = [
+    'https://hd.cdn8-video.com/zxvcbnxcvbnxcvb.gif',
+    'https://hd.cdn8-video.com/zcdzbgfh.gif',
+    'https://hd.cdn8-video.com/wtwterherg.gif',
+    'https://hd.cdn8-video.com/wtqetwy.gif',
+    'https://hd.cdn8-video.com/wrfetwywey.gif',
+    'https://hd.cdn8-video.com/wetergrgh.gif',
+    'https://hd.cdn8-video.com/wegehrthrh.gif',
+    'https://hd.cdn8-video.com/waetryusddfghjkf.gif',
+    'https://hd.cdn8-video.com/srfewhkjh.gif',
+    'https://hd.cdn8-video.com/shjkhlfgdghj.gif',
+    'https://hd.cdn8-video.com/sghs.gif',
+    'https://hd.cdn8-video.com/sghgfdhfjjfj.gif',
+    'https://hd.cdn8-video.com/sghasgh.gif',
+    'https://hd.cdn8-video.com/sgdjhgjsg.gif',
+    'https://hd.cdn8-video.com/sfsehtjuk.gif',
+    'https://hd.cdn8-video.com/sfghsddfg.gif',
+    'https://hd.cdn8-video.com/sfdghjdsfg.gif',
+    'https://hd.cdn8-video.com/segtrhyfk.gif',
+    'https://hd.cdn8-video.com/sdhfgfdsgfhfd.gif',
+    'https://hd.cdn8-video.com/sdhdfjgk.gif',
+    'https://hd.cdn8-video.com/sdhdfjfkg.gif',
+    'https://hd.cdn8-video.com/sdgsdhdgj.gif',
+    'https://hd.cdn8-video.com/sdghjgkhl.gif',
+    'https://hd.cdn8-video.com/sdgfhjsdafg.gif',
+    'https://hd.cdn8-video.com/sdgfdhgfjgk.gif',
+    'https://hd.cdn8-video.com/sdgdjgkhk.gif',
+    'https://hd.cdn8-video.com/sdgdjgkh.gif',
+    'https://hd.cdn8-video.com/sdfshfgkdg.gif',
+    'https://hd.cdn8-video.com/sdfhgmnbvcxzxcvbn.gif',
+    'https://hd.cdn8-video.com/sdfghjreertfcfs.gif',
+    'https://hd.cdn8-video.com/sdfghjklkjhg.gif',
+    'https://hd.cdn8-video.com/sdfghjkjhgfdss.gif',
+    'https://hd.cdn8-video.com/sdffhgkhjgfdsa.gif',
+    'https://hd.cdn8-video.com/rthrhetgwt.gif',
+    'https://hd.cdn8-video.com/rthdhdgdjghjdgdhj.gif',
+    'https://hd.cdn8-video.com/rhgrtjsdfaf.gif',
+    'https://hd.cdn8-video.com/qwtrwyewtw.gif',
+    'https://hd.cdn8-video.com/qrqetwrhy.gif',
+    'https://hd.cdn8-video.com/qetwyhtrjt.gif',
+    'https://hd.cdn8-video.com/ouiytrewertyui.gif',
+    'https://hd.cdn8-video.com/hfjsrefag.gif',
+    'https://hd.cdn8-video.com/hffkdsfshj.gif',
+    'https://hd.cdn8-video.com/hdql1vngakk.gif',
+    'https://hd.cdn8-video.com/grykuhkdgdtj.gif',
+    'https://hd.cdn8-video.com/gjfgshdgjsf.gif',
+    'https://hd.cdn8-video.com/gffjsdfgsj.gif',
+    'https://hd.cdn8-video.com/gfdhsfahsfghs.gif',
+    'https://hd.cdn8-video.com/gbxbzdfsd.gif',
+    'https://hd.cdn8-video.com/fjfkjgkhl.gif',
+    'https://hd.cdn8-video.com/fdsghjasfg.gif',
+    'https://hd.cdn8-video.com/fdghjhgf.gif',
+    'https://hd.cdn8-video.com/fagfshdgsg.gif',
+    'https://hd.cdn8-video.com/erywryher.gif',
+    'https://hd.cdn8-video.com/ertyjxvbcvmesgrh.gif',
+    'https://hd.cdn8-video.com/egrherhrtj.gif',
+    'https://hd.cdn8-video.com/eghfdfasgdhfjgh.gif',
+    'https://hd.cdn8-video.com/efefsdfdg.gif',
+    'https://hd.cdn8-video.com/efefdvdftergfngfh.gif',
+    'https://hd.cdn8-video.com/edghedfsgfdhfh.gif',
+    'https://hd.cdn8-video.com/dsfghjksdfgh.gif',
+    'https://hd.cdn8-video.com/dsfdafdh.gif',
+    'https://hd.cdn8-video.com/drhdjykesgf.gif',
+    'https://hd.cdn8-video.com/dhjgkjjhs.gif',
+    'https://hd.cdn8-video.com/dfhjkkjhgfdsdfg.gif',
+    'https://hd.cdn8-video.com/dfhgfkhl.gif',
+    'https://hd.cdn8-video.com/dfhfkyuyd.gif',
+    'https://hd.cdn8-video.com/dfghjytredfgfx.gif',
+    'https://hd.cdn8-video.com/dfghjvbncxvb.gif',
+    'https://hd.cdn8-video.com/dfghjkjhgfd.gif',
+    'https://hd.cdn8-video.com/dfghjhgfdrtygvc.gif',
+    'https://hd.cdn8-video.com/dfgfhjhgfd.gif',
+    'https://hd.cdn8-video.com/awrweytr.gif',
+    'https://hd.cdn8-video.com/asfshgfksd.gif',
+    'https://hd.cdn8-video.com/asfsgdj.gif',
+    'https://hd.cdn8-video.com/asfeytjuk.gif',
+    'https://hd.cdn8-video.com/asfdghhgfds.gif',
+    'https://hd.cdn8-video.com/asfashtrure.gif',
+    'https://hd.cdn8-video.com/asdfghfdsa.gif',
+    'https://hd.cdn8-video.com/agghheru.gif',
+    'https://hd.cdn8-video.com/agegrhrt.gif',
+    'https://hd.cdn8-video.com/agdjgkg.gif',
+    'https://hd.cdn8-video.com/afshfgjkhd.gif',
+    'https://hd.cdn8-video.com/afagfayhe.gif',
+
+    
+];
+
+// 随机文字
+var grid_data_randomtext = [
+    '榴莲视频',
+    '永久免费',
+    '嫩草影院',
+    '芒果视频',
+    '高清在线',
+    '天堂视频',
+    '家庭教师',
+    '姨母诱惑',
+    '韩国美女',
+    '天天视频',
+    '特级a片',
+    '免费在线',
+    '迪丽热巴',
+    '水果视频',
+    '国产免费',
+    '女大学生',
+    '极品美女',
+    '精品视频',
+    '韩国伦理',
+    '美女裸体',
+    '福利视频',
+    '免费黄色',
+    '大片在线',
+    '九九视频',
+    '樱桃在线',
+    '香蕉视频',
+    '丝瓜视频',
+    '茄子视频',
+    '未满十八',
+    '黄瓜视频',
+    '电影天堂',
+    '国产在线',
+    '教学视频',
+    '年轻母亲',
+    '杨幂做爱',
+    '18岁禁止',
+    '韩国三级',
+    '污污视频',
+    '天天影院',
+    '欧美特黄',
+    '樱桃视频',
+    '日本女优',
+    '猫咪视频',
+    '人与动物',
+    '美女被操',
+    '柠檬视频',
+    '诱惑做爱',
+    '欧美视频',
+    '泡泡影视',
+    '两个女人',
+    '樱花视频',
+    '草草影院',
+    '12岁女孩',
+    '荔枝视频',
+    '在线自拍',
+    '性爱姿势',
+    '狐狸视频',
+    '伦理大片',
+    '娱乐视频',
+    '一女多男',
+    '少女吞精',
+    '熟女人妻',
+    '橙子视频',
+    '体外射精',
+    '萝卜视频',
+    '女性高潮',
+    '内射哭了',
+    '激情性爱',
+    '成人视频',
+    '欧美激情',
+    '双飞经典',
+    '暖暖视频',
+    '91自拍',
+    '职场同事',
+    '强奸表嫂',
+    '女上男下',
+    '亲吻视频',
+    '最美素人',
+    '14岁女孩',
+    '群P乱伦',
+    '野外迷奸',
+    '戏弄美穴',
+    '扛腿抽插',
+    '内射小屄',
+    '暴力虐操',
+    '粉红小穴',
+    '醉酒颜射',
+    '自慰骚穴',
+    '波波视频',
+];
+
+//引入设置
+document.write("<script type='text/javascript' src='/need/setting.js?10'><\/script>");
